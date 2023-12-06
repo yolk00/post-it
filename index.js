94becad3dcb006dfeeccd512094e2bb5797ef96f
yolk00 <136626573+yolk00@users.noreply.github.com>
2023-12-06 15:52:36 -0500

    set up "connections"
    faux db connection to firebase. drafted how Posts will function

--- index.js
+++ index.js
@@ -1,15 +1,71 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL: "",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const postsInDB = ref(database, "posts");
+
 const inputFieldEl = document.getElementById("msg-el");
 const postButtonEl = document.getElementById("post-btn");
-const postsEl = document.getElementById("posts-list-el");
+const postListEl = document.getElementById("posts-list-el");
 
 postButtonEl.addEventListener("click", function () {
   let inputValue = inputFieldEl.value;
 
-  //   push(postItInDB, inputValue)
+  push(postsInDB, inputValue);
+  console.log(`${inputValue} added to database`);
 
   clearInputFieldEl();
 });
 
+onValue(postsInDB, function (snapshot) {
+  if (snapshot.exists()) {
+    let postsArray = Object.entries(snapshot.val());
+
+    clearPostsEl();
+
+    for (let i = 0; i < postsArray.length; i++) {
+      let currentPost = postsArray[i];
+      let currentPostID = currentPost[0];
+      let currentPostValue = currentPost[1];
+
+      appendPostToPostListEl(currentPost);
+    }
+  } else {
+    postListEl.innerHTML = "Not posts here... yet :)";
+  }
+});
+
 function clearInputFieldEl() {
   inputFieldEl.value = "";
+}
+
+function clearPostsEl() {
+  postListEl.innerHTML = "";
+}
+
+function appendPostToPostListEl(post) {
+  let postID = post[0];
+  let postValue = post[1];
+
+  let newEl = document.createElement("li");
+
+  newEl.textContent = postValue;
+
+  newEl.addEventListener("dblclick", function () {
+    let exactLocationOfPostInDB = ref(database, `posts/${postID}`);
+    remove(exactLocationOfPostInDB);
+  });
+
+  postListEl.append(newEl);
 }
